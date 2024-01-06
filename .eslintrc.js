666476fc728089958b3b4a7aa33136a53306add9
dmitrygvl <dmitrygv1994@ya.ru>
2024-01-06 22:03:16 +0300

    Add env

---
+++ .eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['jest', '@typescript-eslint'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    'import/extensions': ['warn', { ts: 'never' }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: __dirname,
+      },
+    ],
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'no-eval': 'off',
+    'no-promise-executor-return': 'off',
+    'no-use-before-define': 'off',
+    'no-restricted-syntax': 'off',
+    'default-param-last': 'off',
+    'import/no-unresolved': 'off',
+  },
+};
